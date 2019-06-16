f16a2a9f53c768de706ee33929afecbd5458e833
Alistair English <alistairsenglish@gmail.com>
2019-06-16 22:54:27 +1000

    got some good stuff going

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"image"
 	"image/color"
+	"math"
 
 	"gocv.io/x/gocv"
 
 	"github.com/alistair-english/DRC2019/internal/pkg/config"
 	"github.com/alistair-english/DRC2019/pkg/cvhelpers"
 )
+
+func b2i(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
 
 func main() {
 
@@ -59,20 +67,24 @@ func main() {
 					cvConfig.LeftLower.H,
 					cvConfig.LeftLower.S,
 					cvConfig.LeftLower.V,
-					0.0),
+					0.0,
+				),
 				channels,
 				rows,
-				cols),
+				cols,
+			),
 
 			cvhelpers.NewHSVMask(
 				gocv.NewScalar(
 					cvConfig.LeftUpper.H,
 					cvConfig.LeftUpper.S,
 					cvConfig.LeftUpper.V,
-					0.0),
+					0.0,
+				),
 				channels,
 				rows,
-				cols),
+				cols,
+			),
 		),
 
 		cvhelpers.NewHSVObject(
@@ -83,20 +95,24 @@ func main() {
 					cvConfig.RightLower.H,
 					cvConfig.RightLower.S,
 					cvConfig.RightLower.V,
-					0.0),
+					0.0,
+				),
 				channels,
 				rows,
-				cols),
+				cols,
+			),
 
 			cvhelpers.NewHSVMask(
 				gocv.NewScalar(
 					cvConfig.RightUpper.H,
 					cvConfig.RightUpper.S,
 					cvConfig.RightUpper.V,
-					0.0),
+					0.0,
+				),
 				channels,
 				rows,
-				cols),
+				cols,
+			),
 		),
 	}
 
@@ -122,17 +138,35 @@ func main() {
 		// Find the HSV objects in the image
 		result := cvhelpers.FindHSVObjects(hsvImg, objects, processMask)
 
-		for _, obj := range result {
-			var colour color.RGBA
-			if obj.Name == LEFT_LINE {
-				colour = color.RGBA{255, 0, 0, 0}
-			} else if obj.Name == RIGHT_LINE {
-				colour = color.RGBA{0, 255, 0, 0}
-			}
+		found := make(map[string]cvhelpers.HSVObjectResult)
 
-			gocv.Rectangle(&sourceImg, obj.BoundingBox, colour, 3)
+		for _, obj := range result {
+			if obj.Area > 50 {
+				found[obj.Name] = obj
+			}
 		}
-		// fmt.Println()
+
+		// fmt.Printf("%v %v\n", found[RIGHT_LINE].BoundingBox.Min.X, found[LEFT_LINE].BoundingBox.Max.X)
+
+		leftLine, lExists := found[LEFT_LINE]
+		rightLine, rExists := found[RIGHT_LINE]
+
+		leftX := b2i(lExists)*leftLine.BoundingBox.Max.X + b2i(!lExists)*0
+		rightX := b2i(rExists)*rightLine.BoundingBox.Min.X + b2i(!rExists)*cols
+
+		horDiff := rightX - leftX
+		horCoord := leftX + horDiff/2
+
+		vertCoord := int(math.Min(
+			float64(b2i(lExists)*leftLine.BoundingBox.Min.Y+b2i(!lExists)*rows),
+			float64(b2i(rExists)*rightLine.BoundingBox.Min.Y+b2i(!rExists)*rows),
+		))
+
+		gocv.Rectangle(&sourceImg, found[RIGHT_LINE].BoundingBox, color.RGBA{255, 255, 0, 0}, 3)
+		gocv.Rectangle(&sourceImg, found[LEFT_LINE].BoundingBox, color.RGBA{0, 0, 255, 0}, 3)
+
+		gocv.Line(&sourceImg, image.Point{cols / 2, rows}, image.Point{horCoord, vertCoord}, color.RGBA{0, 255, 0, 0}, 3)
+		gocv.Circle(&sourceImg, image.Point{horCoord, vertCoord}, 5, color.RGBA{255, 0, 0, 0}, 3)
 
 		// Display source img
 		displayWindow.IMShow(sourceImg)
