e900325053ec1f1ddc1ed88372464d115921ff31
Alistair English <alistairsenglish@gmail.com>
2019-06-22 11:13:52 +1000

    more stuff

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	"github.com/alistair-english/DRC2019/internal/pkg/camera"
+	"fmt"
+
 	"gocv.io/x/gocv"
+
+	"github.com/alistair-english/DRC2019/internal/pkg/camera"
 )
+
+func processImg(img gocv.Mat, motionCtrlChan chan<- bool) {
+	// This is where the image gets processed and then we send a move control struct to the serial.
+	fmt.Println(img.GetIntAt3(500, 500, 4))
+	motionCtrlChan <- true
+}
 
 func main() {
 	// Setup
@@ -13,6 +22,8 @@ func main() {
 	cam := camera.FakeCamera{}
 	camConn := camera.NewConnection(cam, &camImg)
 
+	serialChan := make(chan bool, 100)
+
 	displayWindow := gocv.NewWindow("Display")
 	defer displayWindow.Close()
 
@@ -21,14 +32,18 @@ func main() {
 	for {
 		select {
 		case <-camConn.ImageResult:
-
-			// Code to handle an image is ready
-			// e.g. process img - this realllyyy needs to be a goroutine otherwise huge block
+			// Img is ready to be processed
 			displayWindow.IMShow(camImg)
 			displayWindow.WaitKey(1)
 
+			// Spawn a go routine to do the heavy processing and then talk to serial when its done
+			go processImg(camImg, serialChan)
+
 			// Then request another image
 			camConn.RequestImage()
+		case <-serialChan:
+			// Processing done (wouldn't actually have this here)
+			fmt.Println("Processing done")
 		}
 	}
 }
