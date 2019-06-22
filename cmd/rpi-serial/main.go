ee13562009c3af4fa4c405a301556c39a7ee5a5f
Alistair English <alistairsenglish@gmail.com>
2019-06-22 15:47:18 +1000

    rpi-serial test

--- cmd/rpi-serial/main.go
+++ cmd/rpi-serial/main.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	t "time"
 
-	"github.com/alistair-english/DRC2019/pkg/config"
 	"github.com/tarm/serial"
 )
 
@@ -50,13 +49,10 @@ func main() {
 	log.SetFlags(log.Lmicroseconds) // Set the logging mode to show microseconds
 	log.Printf("-------NEWLOG-------")
 
-	// Get the port from config
-	camConfig := config.GetCameraConfig()
-
 	// Setup serial options
 	log.Printf("Starting rpi-serial")
 	options := serial.Config{
-		Name:        camConfig.Port,     // Just a testing serial port
+		Name:        "/dev/ttyS0",       // Just a testing serial port
 		Baud:        115200,             // Probs the wrong baud rate
 		ReadTimeout: t.Millisecond * 10, // Filler Value
 		Size:        8,
@@ -87,41 +83,41 @@ func main() {
 	}(writeChan)
 
 	// Read goroutine to recieve serial data
-	go func(readChannel chan<- []byte) {
-		for {
-			buf := make([]byte, 255)
-			n, _ := port.Read(buf)
-			if n == 0 {
-				// Failed to read from port in 10ms
-			} else if n >= serialHeaderSize {
-				// We potentially have enought data for serial
-				// Now check for serial sync characters
-				for i := n; i >= serialHeaderSize; i-- {
-					if uint8(buf[i]) == serialSync1 && uint8(buf[i+1]) == serialSync2 {
-						// We are synced up and have an entire packet
-						dataPacket := make([]byte, serialHeaderSize)
-						// Copy the data without sync to the dataPacket buffer
-						n := copy(dataPacket, buf[i:i+serialHeaderSize]) //If we are truncating data, this will be the issue
-						if n != serialHeaderSize {
-							log.Fatal("Somehow we lost count of our buffer")
-						}
-						// We have a serial header, decode it and read in the memory
-						headerPacket := decodeHeaderPacket(dataPacket)
-						if headerPacket.Size == 0 {
-							log.Fatal("No data in packet")
-						}
-						packetBuf := make([]byte, headerPacket.Size)
-						nn, _ := port.Read(packetBuf)
-						if uint8(nn) < headerPacket.Size {
-							log.Fatal("We somehow expected more data than we got")
-						}
-						// Place the raw data on a channel to be handled
-						readChannel <- packetBuf
-					}
-				}
-			}
-		}
-	}(readChan)
+	// go func(readChannel chan<- []byte) {
+	// 	for {
+	// 		buf := make([]byte, 255)
+	// 		n, _ := port.Read(buf)
+	// 		if n == 0 {
+	// 			// Failed to read from port in 10ms
+	// 		} else if n >= serialHeaderSize {
+	// 			// We potentially have enought data for serial
+	// 			// Now check for serial sync characters
+	// 			for i := n; i >= serialHeaderSize; i-- {
+	// 				if uint8(buf[i]) == serialSync1 && uint8(buf[i+1]) == serialSync2 {
+	// 					// We are synced up and have an entire packet
+	// 					dataPacket := make([]byte, serialHeaderSize)
+	// 					// Copy the data without sync to the dataPacket buffer
+	// 					n := copy(dataPacket, buf[i:i+serialHeaderSize]) //If we are truncating data, this will be the issue
+	// 					if n != serialHeaderSize {
+	// 						log.Fatal("Somehow we lost count of our buffer")
+	// 					}
+	// 					// We have a serial header, decode it and read in the memory
+	// 					// headerPacket := decodeHeaderPacket(dataPacket)
+	// 					// if headerPacket.Size == 0 {
+	// 					// 	log.Fatal("No data in packet")
+	// 					// }
+	// 					// packetBuf := make([]byte, headerPacket.Size)
+	// 					// nn, _ := port.Read(packetBuf)
+	// 					// if uint8(nn) < headerPacket.Size {
+	// 					// 	log.Fatal("We somehow expected more data than we got")
+	// 					// }
+	// 					// Place the raw data on a channel to be handled
+	// 					readChannel <- packetBuf
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }(readChan)
 
 	// Goroutine to parse instructions from ESP
 	go func(readChannel <-chan []byte) {
