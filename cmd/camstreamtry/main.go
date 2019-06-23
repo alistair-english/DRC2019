7d5116713bcf347ce9fd82a2809597cd6542d560
Alistair English <alistairsenglish@gmail.com>
2019-06-23 12:16:42 +1000

    .

--- cmd/camstreamtry/main.go
+++ cmd/camstreamtry/main.go
@@ -25,28 +25,38 @@ func main() {
 	}
 
 	cmd.Stderr = os.Stderr
+	readBuff := make([]byte, 4096) // 4 kilobytes of data
 
-	for {
-		readBuff := make([]byte, 4096) // 4 kilobytes of data
-
-		n, err := stdOut.Read(readBuff)
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(n)
-
-		fmt.Println(len(readBuff))
-		fmt.Println(readBuff[0:30])
-
-		// img, err := gocv.IMDecode(data, gocv.IMReadUnchanged)
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
-
-		//displayWindow.IMShow(img)
-		//displayWindow.WaitKey(1)
-		//fmt.Println(len(data))
-		// fmt.Println(img.Size())
-		// img.Close()
+	n, err := stdOut.Read(readBuff)
+	if err != nil {
+		fmt.Println(err)
 	}
+	fmt.Println(n)
+
+	fmt.Println(len(readBuff))
+	fmt.Println(readBuff[0:50])
+
+	// for {
+	// 	readBuff := make([]byte, 4096) // 4 kilobytes of data
+
+	// 	n, err := stdOut.Read(readBuff)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	fmt.Println(n)
+
+	// 	fmt.Println(len(readBuff))
+	// 	fmt.Println(readBuff[0:30])
+
+	// 	// img, err := gocv.IMDecode(data, gocv.IMReadUnchanged)
+	// 	// if err != nil {
+	// 	// 	fmt.Println(err)
+	// 	// }
+
+	// 	//displayWindow.IMShow(img)
+	// 	//displayWindow.WaitKey(1)
+	// 	//fmt.Println(len(data))
+	// 	// fmt.Println(img.Size())
+	// 	// img.Close()
+	// }
 }
