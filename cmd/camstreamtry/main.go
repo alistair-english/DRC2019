bb4327b23c739549aa39157e419a042387cf575f
Alistair English <alistairsenglish@gmail.com>
2019-06-23 13:07:32 +1000

    .

--- cmd/camstreamtry/main.go
+++ cmd/camstreamtry/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
+
+	"gocv.io/x/gocv"
 )
 
+var jpgStart = []byte{0xFF, 0xD8, 0xFF}
+
 func main() {
-	//displayWindow := gocv.NewWindow("Display")
-	//defer displayWindow.Close()
+	displayWindow := gocv.NewWindow("Display")
+	defer displayWindow.Close()
 
 	cmd := exec.Command("raspivid", "-cd", "MJPEG", "-t", "0", "-o", "-")
 
@@ -25,38 +30,61 @@ func main() {
 	}
 
 	cmd.Stderr = os.Stderr
-	readBuff := make([]byte, 4096) // 4 kilobytes of data
 
-	n, err := stdOut.Read(readBuff)
-	if err != nil {
-		fmt.Println(err)
+	readBuff := make([]byte, 4096) // read in stream 4 kilobyte chunks
+	imgBuff := new(bytes.Buffer)
+
+	var currImg []byte
+
+	go func() {
+		for {
+
+			n, err := stdOut.Read(readBuff)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(n)
+			fmt.Println(len(readBuff))
+			fmt.Println(readBuff[0:30])
+
+			for i := 0; i < n; i++ {
+				// check if we found the start of an image
+				if bytes.Compare(readBuff[i:i+len(jpgStart)], jpgStart) == 0 {
+					// we found a new image start point at i
+
+					// write the rest of the old image into the currImg buffer
+					imgBuff.Write(readBuff[0:i])
+
+					if imgBuff.Len() > 0 {
+						// the was already part of an img in here - img must be done
+
+						// Copy the completed image out of the buffer and into the current img
+						cpyImg := make([]byte, imgBuff.Len())
+						copy(cpyImg, imgBuff.Bytes())
+						currImg = cpyImg
+
+						// reset the buffer
+						imgBuff.Reset()
+					}
+
+					imgBuff.Write(readBuff[i:])
+				} else {
+					imgBuff.Write(readBuff)
+				}
+			}
+		}
+	}()
+
+	for {
+		img, err := gocv.IMDecode(currImg, gocv.IMReadUnchanged)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		displayWindow.IMShow(img)
+		displayWindow.WaitKey(1)
+		fmt.Println(len(currImg))
+		fmt.Println(img.Size())
+		img.Close()
 	}
-	fmt.Println(n)
-
-	fmt.Println(len(readBuff))
-	fmt.Println(readBuff[0:50])
-
-	// for {
-	// 	readBuff := make([]byte, 4096) // 4 kilobytes of data
-
-	// 	n, err := stdOut.Read(readBuff)
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 	}
-	// 	fmt.Println(n)
-
-	// 	fmt.Println(len(readBuff))
-	// 	fmt.Println(readBuff[0:30])
-
-	// 	// img, err := gocv.IMDecode(data, gocv.IMReadUnchanged)
-	// 	// if err != nil {
-	// 	// 	fmt.Println(err)
-	// 	// }
-
-	// 	//displayWindow.IMShow(img)
-	// 	//displayWindow.WaitKey(1)
-	// 	//fmt.Println(len(data))
-	// 	// fmt.Println(img.Size())
-	// 	// img.Close()
-	// }
 }
