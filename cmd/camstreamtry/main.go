8e678615bd2694ab0cf0f076ebd71122daa91dcd
Alistair English <alistairsenglish@gmail.com>
2019-06-23 19:17:14 +1000

    Revert "."
    This reverts commit 0e5484d2cb8c62b3f0236ad016d2a33b7acfc343.

--- cmd/camstreamtry/main.go
+++ cmd/camstreamtry/main.go
@@ -12,8 +12,8 @@ import (
 var jpgStart = []byte{0xFF, 0xD8, 0xFF}
 
 func main() {
-	displayWindow := gocv.NewWindow("Display")
-	defer displayWindow.Close()
+	// displayWindow := gocv.NewWindow("Display")
+	// defer displayWindow.Close()
 
 	cmd := exec.Command("raspivid", "-cd", "MJPEG", "-t", "0", "-o", "-")
 
@@ -96,15 +96,11 @@ func main() {
 
 			fmt.Println(img.GetVeciAt(img.Cols()/2, img.Rows()/2))
 
-			go func(img gocv.Mat, imgToClose *gocv.Mat, window *gocv.Window) {
-				displayWindow.IMShow(img)
-				displayWindow.WaitKey(1)
-				fmt.Println(len(currImg))
-				fmt.Println(img.Size())
-				imgToClose.Close()
-				img.Close()
-			}(img, &img, displayWindow)
-
+			// displayWindow.IMShow(img)
+			// displayWindow.WaitKey(1)
+			// fmt.Println(len(currImg))
+			// fmt.Println(img.Size())
+			img.Close()
 		}
 	}
 }
