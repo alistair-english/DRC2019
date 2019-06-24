a4883bf89bcdb4709bca42869a7feec332a9eca6
TomFraser <tr.fraser@bigpond.com>
2019-06-24 18:25:32 +1000

    Yeet

---
+++ cmd/fakelinefollow/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"gocv.io/x/gocv"
+
+	//"github.com/alistair-english/DRC2019/pkg/camera"
+	"github.com/alistair-english/DRC2019/pkg/config"
+	"github.com/alistair-english/DRC2019/pkg/serial"
+)
+
+func processImg(img gocv.Mat, motionCtrlChan chan<- serial.Control) {
+	if img.Rows() == 0 || img.Cols() == 0 {
+		return
+	}
+	// This is where the image gets processed and then we send a move control struct to the serial.
+	fmt.Println(img.GetVeciAt(img.Cols()/2, img.Rows()/2))
+	motionCtrlChan <- serial.Control{
+		Dir: 0,
+		Spd: 50,
+	}
+}
+
+func main() {
+	// Camera Setup
+	// camImg := gocv.NewMat()
+	// defer camImg.Close()
+	// cam, err := camera.NewPiCamera()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// camConn := camera.NewConnection(cam, &camImg)
+
+	// Serial Setup
+	serialConfig := config.GetSerialConfig()
+	ser, _ := serial.NewPiSerial(
+		serialConfig.Port,
+		serialConfig.Baud,
+		time.Duration(serialConfig.TimeoutMs)*time.Millisecond,
+	)
+	serConn, _ := serial.NewConnection(ser)
+	serConn.ControlChan <- serial.Control{
+		Dir: 0,
+		Spd: 50,
+	}
+	time.Sleep(time.Millisecond * 500)
+	serConn.ControlChan <- serial.Control{
+		Dir: 90,
+		Spd: 50,
+	}
+	time.Sleep(time.Millisecond * 500)
+	for i := 0; i < 10; i++ {
+		serConn.ControlChan <- serial.Control{
+			Dir: int8(90 - 9*i),
+			Spd: 50,
+		}
+		time.Sleep(time.Millisecond * 300)
+	}
+	serConn.ControlChan <- serial.Control{
+		Dir: 0,
+		Spd: 50,
+	}
+
+	// displayWindow := gocv.NewWindow("Display")
+	// defer displayWindow.Close()
+
+	// camConn.RequestImage()
+
+	// for {
+	// 	select {
+	// 	case <-camConn.ImageResult:
+	// 		// Img is ready to be processed
+	// 		if camImg.Rows() > 0 || camImg.Cols() > 0 {
+	// 			displayWindow.IMShow(camImg)
+	// 			displayWindow.WaitKey(1)
+	// 		}
+
+	// 		// Spawn a go routine to do the heavy processing and then talk to serial when its done
+	// 		go processImg(camImg, serConn.ControlChan)
+
+	// 		// Then request another image
+	// 		camConn.RequestImage()
+	// 	case pwrReq := <-serConn.PowerReqChan:
+	// 		if pwrReq.ReqType == serial.ForceStop {
+	// 			fmt.Println("shut down!")
+	// 		}
+	// 	}
+	// }
+}
