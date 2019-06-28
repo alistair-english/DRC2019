2d9ca53fccb1413f7cc15c7052b1c353c45774f0
TomFraser <tr.fraser@bigpond.com>
2019-06-28 14:48:45 +1000

    Comments for ease of understanding:

--- bak/cmd/log-test/main.go
+++ bak/cmd/log-test/main.go
@@ -5,12 +5,18 @@ import (
 )
 
 func main() {
+	// Create the logger, not matter how much you do this and where, it will be the same logger instance
 	log := logging.Logger()
+	// Create a default stream and set it
 	log.Init()
+	// Add the rpi-serial.log file stream with a title SERIAL_LOG
 	log.AddStream("rpi-serial.log", "SERIAL_LOG")
+	// Log to the default stream
 	log.Logln("Hello World! (in default)")
+	// Change stream
 	log.ChangeStream("SERIAL_LOG")
+	// Log to the new stream
 	log.Log("From the Logger! %v \n", 10)
-
+	// Log while outputting to stdio
 	log.LogOutln("This should log to terminal: %v", 69)
 }
