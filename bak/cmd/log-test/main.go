1add9fc9b1cb4dbba67a1eefe0cddc71acafa447
TomFraser <tr.fraser@bigpond.com>
2019-06-28 14:43:06 +1000

    Made the logging package much better, stores streams to use later

--- bak/cmd/log-test/main.go
+++ bak/cmd/log-test/main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"github.com/alistair-english/DRC2019/pkg/logging"
-	"os"
 )
 
 func main() {
 	log := logging.Logger()
 	log.Init()
-	f, err := os.OpenFile(os.Getenv("GOPATH")+"/src/github.com/alistair-english/DRC2019/logs/rpi-serial.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		panic(err)
-	}
-	log.SetStream(f, "SERIAL_LOG")
-	log.Logln("Hello World!")
-	log.Log("From the Logger! %v", 10)
+	log.AddStream("rpi-serial.log", "SERIAL_LOG")
+	log.Logln("Hello World! (in default)")
+	log.ChangeStream("SERIAL_LOG")
+	log.Log("From the Logger! %v \n", 10)
+
+	log.LogOutln("This should log to terminal: %v", 69)
 }
