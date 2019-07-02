55431f28996a0927e19bd56cccb8aea398386c90
Alistair English <alistairsenglish@gmail.com>
2019-07-02 22:04:30 +1000

    why is the PID backwards

--- pkg/services/cvservice/basicdrivecontroller.go
+++ pkg/services/cvservice/basicdrivecontroller.go
@@ -58,10 +58,10 @@ func (c *basicDriveController) update(objs []cvhelpers.HSVObjectGroupResult) ser
 
 	ang, _ := c.getTrackAngleAndDriveSpeed(leftLineGroup, rightLineGroup, obstaclesGroup)
 
-	dir := int8(c.controlPID.Update(float64(ang)))
+	// dir := int8(c.controlPID.Update(float64(ang)))
 
 	return serialservice.Control{
-		Dir: dir,
+		Dir: ang,
 		Spd: 100,
 	}
 }
