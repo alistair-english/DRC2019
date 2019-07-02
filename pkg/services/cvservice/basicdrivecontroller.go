ba24f418352240c00faa94a7ec3463413787d86b
Alistair English <alistairsenglish@gmail.com>
2019-07-02 20:21:12 +1000

    obstacle stuff

--- pkg/services/cvservice/basicdrivecontroller.go
+++ pkg/services/cvservice/basicdrivecontroller.go
@@ -90,9 +90,21 @@ func (c *basicDriveController) getTrackAngleAndDriveSpeed(leftLineGroup, rightLi
 		rightBound = rightLine.BoundingBox.Min.X
 	)
 
-	// for _, obj := range obstaclesGroup.Objects {
-	// 	leftDist := obj.BoundingBox.Min.X -
-	// }
+	// Update left and right bounds based on obstacles
+	for _, obj := range obstaclesGroup.Objects {
+		fmt.Println("Obstacle:", obj.BoundingBox.Min.X, "-", obj.BoundingBox.Max.X)
+		leftDist := obj.BoundingBox.Min.X - leftBound
+		rightDist := obj.BoundingBox.Max.X - rightBound
+
+		if leftDist < rightDist {
+			leftBound = obj.BoundingBox.Max.X
+		} else {
+			rightBound = obj.BoundingBox.Min.X
+		}
+
+	}
+
+	fmt.Println("Left Bound:", leftBound, "Right Bound:", rightBound)
 
 	horDiff := rightBound - leftBound
 	horX := leftBound + horDiff/2
